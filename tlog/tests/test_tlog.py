f0870c53e110804ad9ea4181af8c4e49d109f760
m4h7 <m4h7@users.noreply.github.com>
2016-08-02 23:00:48 +0100

    Added another test case.

--- tlog/tests/test_tlog.py
+++ tlog/tests/test_tlog.py
@@ -4,7 +4,7 @@ import io
 import tlog
 
 class TestTlog(TestCase):
-    def test_put_get(self):
+    def test_single_put_get(self):
         keywritten = b'key'
         valuewritten = b'value'
         f = io.BytesIO()
@@ -14,3 +14,14 @@ class TestTlog(TestCase):
         self.assertTrue(keyread == keywritten)
         self.assertTrue(valueread == valuewritten)
 
+    def test_multi_put_get(self):
+        f = io.BytesIO()
+        for n in range(1000):
+            key = str(n)
+            value = str(n + 1)
+            tlog.put(f, key.encode('ascii'), value.encode('ascii'))
+        f.seek(0)
+        for n in range(1000):
+            key, value = tlog.get(f)
+            self.assertTrue(int(key.decode('ascii')) == n)
+            self.assertTrue(int(value.decode('ascii')) == n + 1)
